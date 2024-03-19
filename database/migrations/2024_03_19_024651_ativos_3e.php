b0c5d73e2c7551efd7c8b965c610eea594962372
henricovisky <henriquemn2.1@gmail.com>
2024-03-19 09:20:43 -0300

    first commit

---
+++ database/migrations/2024_03_19_024651_ativos_3e.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ativos', function (Blueprint $table) { /* Criando tabela de ativos da 3e */
+          $table->id();
+          $table->string('nome');
+          $table->longText('descricao');
+          $table->string('categoria');
+          $table->date('data_aquisicao');
+          $table->decimal('valor', 10, 2);
+          $table->string('localizacao');
+          $table->timestamps(); /* cria coluna com data de criação e atualização */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ativos_3e');
+    }
+};
